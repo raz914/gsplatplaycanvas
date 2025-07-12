af6d4b7b54131c5f6eeec802412e4936e7e83a32
raz914 <raziulhassan914@gmail.com>
2025-07-12 05:29:00 +0500

    css fixed for sidebar

--- src/components/RoomPanel.jsx
+++ src/components/RoomPanel.jsx
@@ -3,23 +3,27 @@ import closeIcon from '../assets/closeIcon.png';
 export default function RoomPanel({ title, isOpen, onClose, rooms }) {
   return (
     <div
-      className={`fixed top-0 right-0 h-full w-72 bg-black/80 shadow-lg transform transition-transform duration-300 ease-in-out z-50 ${
+      className={`fixed top-0 right-0 h-full w-1/4 bg-black/80 shadow-lg transform transition-transform duration-300 ease-in-out z-50 ${
         isOpen ? 'translate-x-0' : 'translate-x-full'
       }`}
     >
       {/* Header with Close Button */}
-      <div className="p-2 border-b flex justify-between items-center">
-        <h2 className="font-agipo text-4xl font-bold text-white">{title}</h2>
-        <button
-          onClick={onClose}
-          className="w-10 h-10 p-1 rounded-xl bg-transparent hover:bg-[#cda632] transition-all duration-200 flex items-center justify-center"
-        >
-          <img
-            src={closeIcon}
-            alt="Close"
-            className="w-full h-full object-contain transition-transform duration-200 hover:scale-110"
-          />
-        </button>
+      <div className="p-4">
+        <div className="flex justify-end ">
+          <button
+            onClick={onClose}
+            className="w-10 h-10 p-1 rounded-xl bg-transparent hover:bg-[#cda632] transition-all duration-200 flex items-center justify-center"
+          >
+            <img
+              src={closeIcon}
+              alt="Close"
+              className="w-full h-full object-contain transition-transform duration-200 hover:scale-110"
+            />
+          </button>
+        </div>
+        <div className="flex justify-center">
+          <h2 className="font-agipo text-6xl font-bold text-white">{title}</h2>
+        </div>
       </div>
 
       {/* Room Buttons */}
@@ -28,7 +32,7 @@ export default function RoomPanel({ title, isOpen, onClose, rooms }) {
           <button
             key={idx}
             onClick={() => console.log(`Clicked: ${room.name}`)} // You can replace this with your action
-            className="flex flex-col items-center bg-black p-2 transform transition-transform duration-200 hover:scale-105"
+            className="flex flex-col items-center bg-transparent p-4 min-h-[120px] w-full transform transition-transform duration-200 hover:scale-105"
           >
             <img
               src={room.image}
